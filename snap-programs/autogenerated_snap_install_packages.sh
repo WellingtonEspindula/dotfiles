402ca9e1b8dc95424e4aff441e21575a4b733007
Wellington Espindula <wellington.m.espindula@gmail.com>
2020-12-04 14:47:15 -0300

    added my first config files

---
+++ snap-programs/autogenerated_snap_install_packages.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+sudo snap install android-studio --classic
+			barrier
+			cast-web-api
+			chromium-ffmpeg
+			clion --classic
+			code --classic
+			core --core
+			core18 --base
+			core20 --base
+			discord
+			gitkraken --classic
+			glimpse-editor
+			gnome-3-28-1804
+			gtk-common-themes
+			gtk2-common-themes
+			intellij-idea-ultimate --classic
+			kde-frameworks-5-qt-5-14-core18
+			mailspring
+			micropad
+			netbeans --classic
+			node --classic
+			okular
+			opera
+			poddr
+			postman
+			simplenote
+			snapd --snapd
+			sosumi
+			spotify
+			sublime-text --classic
+			todoist
+			umbrello
+			wps-office
+			zotero-snap
+			
---
+++ snap-programs/snap_install_packages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+sudo snap install android-studio --classic
+			barrier
+			clion --classic
+			code --classic
+			discord
+			gitkraken --classic
+			glimpse-editor
+			intellij-idea-ultimate --classic
+			mailspring
+			micropad
+			netbeans --classic
+			node --classic
+			okular
+			opera
+			poddr
+			postman
+			simplenote
+			spotify
+			sublime-text --classic
+			todoist
+			wps-office
+			
